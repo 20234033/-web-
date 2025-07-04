f9f993cb91e8e044001489368f488ab9ec992b53
20234075 <20234075@st.denpa.jp>
2025-07-04 12:20:42 +0900

    getDistanceKmを削除し、localStrageのlastDistanceから取得するように変更

--- public/js/result.js
+++ public/js/result.js
@@ -1,16 +1,3 @@
-
-function getDistanceKm(lat1, lon1, lat2, lon2) {
-  const R = 6371;
-  const toRad = deg => deg * (Math.PI / 180);
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
-  const a = Math.sin(dLat / 2) ** 2 +
-            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-            Math.sin(dLon / 2) ** 2;
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c;
-}
-
 window.addEventListener('DOMContentLoaded', async () => {
 
   const resultMap = L.map('result-map').setView([35.7, 139.7], 10);
@@ -28,7 +15,7 @@ window.addEventListener('DOMContentLoaded', async () => {
     return;
   }
 
-  const dist = getDistanceKm(answer.lat, answer.lng, correct.lat, correct.lng);
+  const dist = JSON.parse(localStorage.getItem('lastDistance'));
   const rawScore = Math.max(0, 5000 - Math.round(dist));
   const score = Math.round((rawScore / 5000) * 100);
   localStorage.setItem('lastScore', score.toString());

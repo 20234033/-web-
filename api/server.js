ccc22d3647e1a1900b47c61f22b4c9cd0025e29c
20234075 <20234075@st.denpa.jp>
2025-10-17 12:31:47 +0900

    DB接続プールを直書きから、envから引っ張ってくるように変更

--- api/server.js
+++ api/server.js
@@ -35,11 +35,11 @@ const SECRET_KEY = process.env.SECRET_KEY || 'your-default-secret';
 
 // ✅ DB接続プール（poolは後で使えるようにmodule.exportsしてもOK）
 const pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'geoapp',
-  password: 'Password',
-  database: 'website',
-  connectionLimit: 5
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 5 
 });
 
 // 📁 パス定義

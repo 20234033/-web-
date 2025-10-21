34928603e8b37d83b07675a14e60ac02d25f0fc6
20234033 <nkc20234033@st.denpa.jp>
2025-10-21 11:59:10 +0900

    一部修正

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: 'sightseeing-app',
       script: 'api/server.js',
+      instances: 1,          // ← ここを1
+      exec_mode: "fork",     // ← fork モード
       instances: 1,               // または 'max'（CPUコア数）
       autorestart: true,         // クラッシュ時に自動再起動
       watch: false,              // ファイル変更を監視しない
